2ecd52f62663dfdc13923d91ce89b546b102677c
Tosin Akinosho <takinosh@redhat.com>
2023-06-29 09:52:52 -0700

    Update deploy.sh

--- gitops/deploy.sh
+++ gitops/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 oc create namespace openshift-gitops
-oc create -k gitops/cluster-config/openshift-gitops
+oc create -k cluster-config/openshift-gitops
 sleep 30s
-oc apply -k gitops/cluster-config/openshift-gitops
+oc apply -k cluster-config/openshift-gitops
