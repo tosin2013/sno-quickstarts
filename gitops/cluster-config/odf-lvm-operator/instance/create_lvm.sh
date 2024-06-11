04b79a0b7e75cf30df936f39a0e6ff85b4174ca0
Tosin Akinosho <takinosh@redhat.com>
2024-06-11 13:12:16 -0400

    updating hostpath

--- gitops/cluster-config/odf-lvm-operator/instance/create_lvm.sh
+++ gitops/cluster-config/odf-lvm-operator/instance/create_lvm.sh
@@ -8,3 +8,10 @@ lvcreate -n lv-vgmcg -l 100%FREE vgmcg
 mkfs.xfs /dev/vgmcg/lv-vgmcg
 vgdisplay 
 mkdir lv-vgmcg
+
+
+
+sudo wipefs -af /dev/vdb
+sudo sgdisk --zap-all /dev/vdb
+sudo dd if=/dev/zero of=/dev/vdb bs=1M count=100 oflag=direct,dsync
+sudo blkdiscard /dev/vdb
